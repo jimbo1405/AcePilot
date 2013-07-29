9572b46c5929f3856bb1ae33209e3dd3301b64e4
Chun-Ping Wang <a0958324790@gmail.com>
2013-07-29 18:37:06 +0800

    add button selector...

--- gen/com/demo/acepilot/R.java
+++ gen/com/demo/acepilot/R.java
@@ -19,43 +19,59 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int clock74x74=0x7f020000;
-        public static final int coin32x32=0x7f020001;
-        public static final int coin74x74=0x7f020002;
-        public static final int ic_launcher=0x7f020003;
-        public static final int lvl_01=0x7f020004;
-        public static final int lvl_02=0x7f020005;
-        public static final int lvl_03=0x7f020006;
-        public static final int lvl_04=0x7f020007;
-        public static final int lvl_05=0x7f020008;
-        public static final int lvl_06=0x7f020009;
-        public static final int lvl_07=0x7f02000a;
-        public static final int lvl_08=0x7f02000b;
-        public static final int lvl_09=0x7f02000c;
-        public static final int lvl_10=0x7f02000d;
-        public static final int lvl_11=0x7f02000e;
-        public static final int lvl_12=0x7f02000f;
-        public static final int normal_bullet=0x7f020010;
-        public static final int opaque418x178=0x7f020011;
-        public static final int soundclose40x40=0x7f020012;
-        public static final int soundopen40x40=0x7f020013;
-        public static final int su_30_flanker=0x7f020014;
-        public static final int talk74x74=0x7f020015;
+        public static final int btn_backmain_normal=0x7f020000;
+        public static final int btn_backmain_press=0x7f020001;
+        public static final int btn_backmain_selector=0x7f020002;
+        public static final int btn_continue_normal=0x7f020003;
+        public static final int btn_continue_press=0x7f020004;
+        public static final int btn_continue_selector=0x7f020005;
+        public static final int btn_highscore_normal=0x7f020006;
+        public static final int btn_highscore_press=0x7f020007;
+        public static final int btn_highscore_selector=0x7f020008;
+        public static final int btn_quit_normal=0x7f020009;
+        public static final int btn_quit_press=0x7f02000a;
+        public static final int btn_quit_selector=0x7f02000b;
+        public static final int btn_start_normal=0x7f02000c;
+        public static final int btn_start_press=0x7f02000d;
+        public static final int btn_start_selector=0x7f02000e;
+        public static final int clock74x74=0x7f02000f;
+        public static final int coin32x32=0x7f020010;
+        public static final int coin40x40=0x7f020011;
+        public static final int coin74x74=0x7f020012;
+        public static final int ic_launcher=0x7f020013;
+        public static final int lvl_01=0x7f020014;
+        public static final int lvl_02=0x7f020015;
+        public static final int lvl_03=0x7f020016;
+        public static final int lvl_04=0x7f020017;
+        public static final int lvl_05=0x7f020018;
+        public static final int lvl_06=0x7f020019;
+        public static final int lvl_07=0x7f02001a;
+        public static final int lvl_08=0x7f02001b;
+        public static final int lvl_09=0x7f02001c;
+        public static final int lvl_10=0x7f02001d;
+        public static final int lvl_11=0x7f02001e;
+        public static final int lvl_12=0x7f02001f;
+        public static final int normal_bullet=0x7f020020;
+        public static final int opaque418x178=0x7f020021;
+        public static final int soundclose40x40=0x7f020022;
+        public static final int soundopen40x40=0x7f020023;
+        public static final int su_30_flanker=0x7f020024;
+        public static final int talk74x74=0x7f020025;
     }
     public static final class id {
         public static final int RelativeLayout1=0x7f0a0015;
         public static final int RelativeLayout2=0x7f0a0016;
         public static final int RelativeLayout3=0x7f0a0018;
         public static final int button1=0x7f0a0004;
-        public static final int button2=0x7f0a0008;
-        public static final int button3=0x7f0a0005;
-        public static final int button4=0x7f0a0006;
+        public static final int button2=0x7f0a0006;
+        public static final int button3=0x7f0a0008;
+        public static final int button4=0x7f0a0009;
         public static final int editText1=0x7f0a0011;
         public static final int game_result_layout=0x7f0a000a;
         public static final int gl_layout=0x7f0a0000;
         public static final int high_score_layout=0x7f0a000e;
         public static final int imageView1=0x7f0a0003;
-        public static final int imageView2=0x7f0a0009;
+        public static final int imageView2=0x7f0a0007;
         public static final int imageView3=0x7f0a000c;
         public static final int imageView4=0x7f0a000d;
         public static final int linear_layout1=0x7f0a0012;
@@ -64,7 +80,7 @@ public final class R {
         public static final int my_listview_layout=0x7f0a0010;
         public static final int progressBar1=0x7f0a0017;
         public static final int textView1=0x7f0a0002;
-        public static final int textView2=0x7f0a0007;
+        public static final int textView2=0x7f0a0005;
         public static final int textView3=0x7f0a000b;
         public static final int text_sec=0x7f0a0013;
         public static final int text_survive=0x7f0a0014;
@@ -96,12 +112,17 @@ public final class R {
         public static final int hign_score=0x7f060009;
         public static final int level=0x7f060008;
         public static final int menu_settings=0x7f060002;
+        public static final int need=0x7f06000f;
+        public static final int no=0x7f060011;
         public static final int pause=0x7f060005;
         public static final int quit=0x7f06000c;
         public static final int resume=0x7f060004;
         public static final int revive=0x7f060006;
         public static final int sec=0x7f06000b;
         public static final int start=0x7f060003;
+        public static final int survive=0x7f06000e;
+        public static final int want_to_revive=0x7f06000d;
+        public static final int yes=0x7f060010;
     }
     public static final class style {
         /** 
