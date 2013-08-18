7e4da90ccb778526d7267d4884c24a94f4b52994
ctku_vaio <ku.chung.ta@gmail.com>
2013-08-18 13:06:14 -0400

    remove flush to speed up

--- src/com/demo/acepilot/MyRender.java
+++ src/com/demo/acepilot/MyRender.java
@@ -12,11 +12,11 @@ import android.util.Log;
 public class MyRender implements Renderer{	
 	public static float player_positionX,player_positionY;	//	
 	public static float x_screen=0,y_screen=0;				//視窗的寬與高	
-	
+
 	public static boolean isDie;							//是否被擊中
 	public static long timePrevious;						//
 	public static boolean drawControlFlag=true;				//藉由控制這個屬性來達到revive時飛機閃爍
-	
+
 	private Square square;
 	private Bullet bullet;
 	private Coin coin;
@@ -27,7 +27,7 @@ public class MyRender implements Renderer{
 	private long timeNow,timeBetweenFrame;
 	private int count;										//第幾個frame
 	private ProbeCircle[] pCircleArray = new ProbeCircle[MyConstant.AIRPLANE_PROBE_PT.length];
-	
+
 	 public MyRender() {
 		 Log.d("ABC","MyRender()...");
 		 square = new Square(MyConstant.AIRPLANE_W, MyConstant.AIRPLANE_H);
@@ -37,7 +37,7 @@ public class MyRender implements Renderer{
 		 isDie=false;			//初始、重置為false，因為MainActivity的onCreate()比onResume()早呼叫，
 	 }							//所以onResume()裡抓到的isDie就是重置過的
 
-	
+
 	@Override
 	public void onDrawFrame(GL10 gl) {						
 		timeNow=System.currentTimeMillis();
@@ -60,7 +60,7 @@ public class MyRender implements Renderer{
 			if(starList.size() < MyConstant.STAR_NUM){
 				prepareBGStar();
 			}
-			
+
 			//畫背景星星
 			for(int i =0 ; i < starList.size() ; i++){
 				Star tmpStar = starList.get(i);
@@ -74,15 +74,15 @@ public class MyRender implements Renderer{
 				gl.glPopMatrix();
 				tmpStar.setStar_positionY(tmpStar.getStar_positionY() - tmpStar.getStar_velocity()*timeBetweenFrame/1000);
 			}
-			
+
 			gl.glColor4f(1.0f, 1.0f, 1.0f, 0.5f);	//還原畫筆為白色
-			
+
 			//每隔幾個Frame準備1個coin，有時會有連續產生2個甚至3個的狀況，因為timeBetweenFrame會變動，可能造成接近的frame餘數為1的情形
 			if((count % (Math.ceil(MyConstant.COIN_TIME_INTERVAL*1000/timeBetweenFrame))) == 1 &&	
 					coinList.size() < MyConstant.COIN_NUM){															
 				prepareCoin(gl);
 			}
-			
+
 			//this for-loop is used to draw coin.
 			for(int i=0;i<coinList.size();i++){
 				Coin c=coinList.get(i);
@@ -90,24 +90,22 @@ public class MyRender implements Renderer{
 				gl.glPushMatrix();
 				gl.glTranslatef((float)c.getCoin_positionX(), (float)c.getCoin_positionY(), 0);
 				c.draw(gl);
-				gl.glFlush();	//*******
 				gl.glPopMatrix();
 			}
-			
+
 			//畫飛機
 			gl.glPushMatrix();													//儲存目前gl狀態		 
 			gl.glTranslatef(player_positionX, player_positionY, 0);
 			gl.glRotatef(-45, 0, 0, 1);
 			if(drawControlFlag)													//******
 				square.draw(gl);		  										//畫出方形
-			gl.glFlush();	//*********
 			gl.glPopMatrix();													//回到上一個gl儲存點的狀態
-			  		  		  		  			
+
 			if((count % (Math.ceil(MyConstant.BULLET_TIME_INTERVAL*1000/timeBetweenFrame))) == 1 &&	//每隔幾個Frame準備1顆子彈
 					bulletList.size() < MyConstant.BULLET_NUM){															
 				prepareBullet(gl);
 			}
-						
+
 			//this for-loop is used to draw bullet.
 			for(int i=0;i<bulletList.size();i++){														  						  
 					  Bullet b=bulletList.get(i);
@@ -115,7 +113,6 @@ public class MyRender implements Renderer{
 					  gl.glPushMatrix();			  
 					  gl.glTranslatef((float)(b.getBullet_positionX()), (float)(b.getBullet_positionY()), 0);	//平移
 					  b.draw(gl);
-					  gl.glFlush();	//*********
 					  gl.glPopMatrix();					 
 					  checkDie(b);										  
 					  b.setBullet_positionX(b.getBullet_positionX() - 
@@ -176,23 +173,23 @@ public class MyRender implements Renderer{
 		gl.glEnable(GL10.GL_DEPTH_TEST);
 		// GL_LEQUAL深度函式測試
 		gl.glDepthFunc(GL10.GL_LEQUAL);
-		
+
 	    gl.glEnable(GL10.GL_ALPHA_TEST);  // *****Enable Alpha Testing (To Make BlackTansparent)  	    
 	    gl.glAlphaFunc(GL10.GL_GREATER,0.1f);  // *****Set Alpha Testing (To Make Black Transparent)  
-		
+
 		//方形載入材質
 		square.loadTexture(gl);
 //		bullet.loadTexture(gl);
 //		bulletList=new ArrayList<Bullet>();	//***搬到onDrawFrame()
 //		starList=new ArrayList<Star>();
 	}
-	
+
 	 public void setBitmap(Bitmap bitmap1,Bitmap bitmap2,Bitmap bitmap3) {
 		 square.setBitmap(bitmap1);
 		 bullet.setBitmap(bitmap2);
 		 coin.setBitmap(bitmap3);
 	 }
-	
+
 	//暫停後、繼續遊戲時必須為當下已存在bulletList內的子彈重新給他材質，因為呼叫onResume()事實上GL10的物件gl已經是新的
 	private void reLoadBulletTexture(GL10 gl){
 		for(int i=0;i<bulletList.size();i++){
@@ -200,7 +197,7 @@ public class MyRender implements Renderer{
 			b.loadTexture(gl);
 		}
 	}
-	
+
 	//暫停後、繼續遊戲時必須為當下已存在coinList內的coin重新給他材質，因為呼叫onResume()事實上GL10的物件gl已經是新的
 	private void reLoadCoinTexture(GL10 gl){
 		for(int i=0;i<coinList.size();i++){
@@ -208,7 +205,7 @@ public class MyRender implements Renderer{
 			c.loadTexture(gl);
 		}
 	}
-	 
+
 	//準備子彈
 	private void prepareBullet(GL10 gl){															
 		Bullet b=new Bullet(MyConstant.BULLET_W, MyConstant.BULLET_H);
@@ -228,12 +225,12 @@ public class MyRender implements Renderer{
 		b.setBulletFlyAngle(tmpFlyAngle);
 		bulletList.add(b);
 	}		
-	
+
 	//消除飛出圓周之外的子彈，傳入整數i表示當前bulletList的index值
 	private void deleteBullet(Bullet b,int i){
 		double tmpFly=b.getBullet_totalFly();        //當前子彈的飛行總位移
 		double tmpRadius=radius;                     //將半徑從像素轉換成座標上的單位
-		
+
 		//if判斷子彈必須在螢幕外 且 至少飛了半徑長度的距離才能從list刪掉
 		if(Math.abs(b.getBullet_positionX()) > (0.5*x_screen) || 
 				Math.abs(b.getBullet_positionY()) > (0.5*y_screen)){
@@ -241,7 +238,7 @@ public class MyRender implements Renderer{
 				bulletList.remove(i);
 		}
 	}
-	
+
 	//get pCircleArray.
 	private void getProbeCircleArray(){
 		for(int i=0;i<MyConstant.AIRPLANE_PROBE_PT.length;i++){
@@ -250,11 +247,11 @@ public class MyRender implements Renderer{
 					MyConstant.AIRPLANE_PROBE_PT[i][2] * MyConstant.AIRPLANE_W);
 		}
 	}
-	
+
 	//測試是否被子彈擊中
 	private void checkDie(Bullet b){
 		getProbeCircleArray();
-		
+
 		//偵測碰撞迴圈
 		for(int i=0;i<pCircleArray.length;i++){			
 				ProbeCircle tmpPC=pCircleArray[i];
@@ -267,7 +264,7 @@ public class MyRender implements Renderer{
 				}					
 		}
 	}
-	
+
 	//初始背景星星
 	private void initBGStar(){
 		for(int i=0 ; i < MyConstant.STAR_NUM ; i++){
@@ -281,7 +278,7 @@ public class MyRender implements Renderer{
 			starList.add(tmpSatr);
 		}		
 	}
-	
+
 	//準備(補充)星星
 	private void prepareBGStar(){
 		Star tmpSatr = new Star(MyConstant.BGSTAR_W, MyConstant.BGSTAR_H);
@@ -292,14 +289,14 @@ public class MyRender implements Renderer{
 		tmpSatr.setColorArray((float)Math.random(), (float)Math.random(), (float)Math.random(), 0.5f);			
 		starList.add(tmpSatr);
 	}
-	
+
 	//刪除跑到螢幕外的星星
 	private void delBGStarOutOfScreen(Star tmpStar, int i){
 		if(tmpStar.getStar_positionY() < (-y_screen/2)){
 			starList.remove(i);
 		}
 	}
-	
+
 	//準備(補充)coin
 	private void prepareCoin(GL10 gl){
 		Coin tmpCoin = new Coin(MyConstant.COIN_W, MyConstant.COIN_H);
@@ -309,7 +306,7 @@ public class MyRender implements Renderer{
 		tmpCoin.loadTexture(gl);
 		coinList.add(tmpCoin);
 	}
-	
+
 	//刪除吃掉的coin
 	private void delEatenCoin(Coin c,int index){
 		getProbeCircleArray();		
